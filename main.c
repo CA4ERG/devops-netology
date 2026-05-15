f07edf69e989ece9195e01ce5a8f470a92ecd023
vlad dodonov <105577804+CA4ERG@users.noreply.github.com>
2026-05-16 01:25:03 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
+#include <locale.h>
+int main() {
+	setlocale (LC_ALL, "RUSSIAN");
+    double a, b, c;
+    double D, x1, x2;
+    a = 1.0;
+    b = 3.0;
+    c = 2.0;
+    printf("Уравнение: %.2f*x^2+%.2f*x+%.2f=0\n", a, b, c);
+    D = pow(b, 2)-4*a*c;
+    printf("Дискриминант: D= %.2f\n", D);
 
-int main(int argc, char *argv[])
-{
-  
-  system("PAUSE");	
-  return 0;
+    if (D>0) {
+        x1=(-b+sqrt(D))/(2*a);
+        x2=(-b-sqrt(D))/(2*a);
+        printf("Два корня:\n");
+        printf("x1= %.4f\n",x1);
+        printf("x2= %.4f",x2);
+    }
+    else if (D==0) {
+        x1=-b/(2*a);
+        printf("Корень: x1= %.4f\n",x1);
+    }
+    else {
+        printf("Корней нет");
+    }
+    return 0;
 }
+
